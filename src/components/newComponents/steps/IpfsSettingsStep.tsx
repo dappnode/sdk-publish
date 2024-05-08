d650757228d7553355f1e5d44e5b4a9c6a15f1c7
mateumiralles <mateumiralles714@gmail.com>
2024-05-08 11:04:16 +0200

    Ipfs settings copy updated

--- src/components/newComponents/steps/IpfsSettingsStep.tsx
+++ src/components/newComponents/steps/IpfsSettingsStep.tsx
@@ -20,29 +20,42 @@ export default function IpfsSettingsStep({
     <BaseCard hasBack={() => stepper.setter(2)}>
       <Title title={"Edit IPFS settings"} />
       <p>
-        {" "}
-        You can edit your IPFS settings specifing multiple URLs, to facilitate
-        propagation of the signed release
+        You can edit your IPFS settings regarding the propagation of the
+        releases. If not specified, the default values shown below will be
+        applied.
       </p>
       <div className="text-text-purple">IPFS API URLs</div>
-      <textarea
-        className={
-          " rounded-2xl bg-background-color p-3  focus:outline-focused-purple "
-        }
-        placeholder="https://infura-ipfs.io:5001 &#13;&#10;http://your-own-ipfs-node:5001"
-        value={ipfsApiUrls}
-        onChange={(e) => setIpfsApiUrls(e.target.value)}
-      />{" "}
+      <div className="flex flex-col">
+        <p className="text-sm">
+          Fulfill with multiple API URLs, to facilitate propagation of the
+          signed release
+        </p>
+        <textarea
+          className={
+            " rounded-2xl bg-background-color p-3  focus:outline-focused-purple "
+          }
+          placeholder="https://infura-ipfs.io:5001 &#13;&#10;http://your-own-ipfs-node:5001"
+          value={ipfsApiUrls}
+          onChange={(e) => setIpfsApiUrls(e.target.value)}
+        />{" "}
+      </div>
       {/* // TODO: IPFS gateway logic */}
       <div className="text-text-purple">IPFS Gateway URLs</div>
-      <textarea
-        className={
-          " rounded-2xl bg-background-color p-3  focus:outline-focused-purple "
-        }
-        placeholder="https://gateway-dev.ipfs.dappnode.io"
-        // value={ipfsApiUrls}
-        // onChange={(e) => setIpfsApiUrls(e.target.value)}
-      />{" "}
+      <div className="flex flex-col">
+        <p className="text-sm">
+          Fulfill with multiple Gateway URLs to ensure that the IPFS hash has
+          propagated or not.
+        </p>
+        <p></p>
+        <textarea
+          className={
+            " rounded-2xl bg-background-color p-3  focus:outline-focused-purple "
+          }
+          placeholder="https://gateway-dev.ipfs.dappnode.io"
+          // value={ipfsApiUrls}
+          // onChange={(e) => setIpfsApiUrls(e.target.value)}
+        />
+      </div>
     </BaseCard>
   );
 }
